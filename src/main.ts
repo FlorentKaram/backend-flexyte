317d81a09ec116a682e16969c83bbe541c87a924
FlorentKaram <85687603+FlorentKaram@users.noreply.github.com>
2024-05-14 09:16:48 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
     }
   ));
   app.setGlobalPrefix('backendFlexyte/');
+  app.enableCors();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('backendFlexyte/' + 'api', app, document);
 
