c4122744067afaff8f81cfe64d7704497a7112f7
GuyMickael <mickael.guy.pro@gmail.com>
2024-09-11 15:18:53 +0200

    correcting my error

--- src/restaurant/services/mongoose-restaurant.service.ts
+++ src/restaurant/services/mongoose-restaurant.service.ts
@@ -1,45 +1,50 @@
-import { HydratedDocument, Model } from "mongoose";
-import { InjectModel } from "@nestjs/mongoose";
-import { Injectable } from "@nestjs/common";
-import { RestaurantDataGateway } from "../interface/restaurant.interface";
-import { CreateRestaurantDto } from "../dto/createRestaurant.dto";
-import { Restaurant } from "../restaurant.model";
+import { HydratedDocument, Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { RestaurantDataGateway } from '../interface/restaurant.interface';
+import { CreateRestaurantDto } from '../dto/createRestaurant.dto';
+import { Restaurant } from '../restaurant.model';
 
 @Injectable()
 export class MongooseRestaurant implements RestaurantDataGateway {
-    constructor(@InjectModel('restaurants') private readonly restaurantModel: Model<Restaurant>) { }
+  constructor(
+    @InjectModel('restaurants')
+    private readonly restaurantModel: Model<Restaurant>,
+  ) {}
 
-    getRestaurantByEmail = async (email: string) => {
-        return this.restaurantModel.findOne({ email: email });
-    };
-    
-    getRestaurantByCompanyName = async  (companyName: string) => {
-        return this.restaurantModel.findOne({ companyName: companyName });
+  getAllRestaurants = async () => {
+    return this.restaurantModel.find();
+  };
 
-    };
+  getRestaurantByEmail = async (email: string) => {
+    return this.restaurantModel.findOne({ email: email });
+  };
 
-    createRestaurant = (restaurant: CreateRestaurantDto) => {
-        return new this.restaurantModel(restaurant);
-    };
+  getRestaurantByCompanyName = async (companyName: string) => {
+    return this.restaurantModel.findOne({ companyName: companyName });
+  };
 
-    createAndSaveRestaurant = async (restaurant: CreateRestaurantDto) => {
-        return this.restaurantModel.create(restaurant);
-    };
+  createRestaurant = (restaurant: CreateRestaurantDto) => {
+    return new this.restaurantModel(restaurant);
+  };
 
-    createFirstRestaurant = async (restaurant: Restaurant) => {
-        return this.restaurantModel.create(restaurant);
-    };
+  createAndSaveRestaurant = async (restaurant: CreateRestaurantDto) => {
+    return this.restaurantModel.create(restaurant);
+  };
 
-    updateRestaurant = async (email: string, restaurant: Restaurant) => {
-        return this.restaurantModel.findOneAndUpdate({ email: email }, restaurant);
-    };
+  createFirstRestaurant = async (restaurant: Restaurant) => {
+    return this.restaurantModel.create(restaurant);
+  };
 
-    deleteRestaurantByEmail = async (email: string) => {
-        return this.restaurantModel.findOneAndDelete({ email: email });
-    };
+  updateRestaurant = async (email: string, restaurant: Restaurant) => {
+    return this.restaurantModel.findOneAndUpdate({ email: email }, restaurant);
+  };
 
-    saveRestaurant = async (restaurant: HydratedDocument<Restaurant>) => {
-        return restaurant.save();
-    }
+  deleteRestaurantByEmail = async (email: string) => {
+    return this.restaurantModel.findOneAndDelete({ email: email });
+  };
 
+  saveRestaurant = async (restaurant: HydratedDocument<Restaurant>) => {
+    return restaurant.save();
+  };
 }
